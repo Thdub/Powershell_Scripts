08632a0c8dcb9cdb688a944565b6bd38db1390e7
Thomas Dubreuil <37347541+Thdub@users.noreply.github.com>
2019-06-09 11:23:34 +0200

    Update DiskWriteCaching.ps1

--- DiskWriteCaching.ps1
+++ DiskWriteCaching.ps1
@@ -1,4 +1,13 @@
-﻿# Cmdlet
+# Self-elevate the script
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+ if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+  $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+  Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+  Exit
+ }
+}
+
+# Cmdlet
 [CmdletBinding()]
 Param(
 [Parameter(Mandatory=$True)]
